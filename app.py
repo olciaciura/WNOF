b1528c4752287a8be61b79ec6d54af9c0fc13316
Ciura <aleksandra.ciura@capgemini.com>
2022-11-30 15:44:55 +0100

    wrrr

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 class DataLoader():
     def result_loader(self, name):
         results = pd.read_csv(name, ';')
-        results_better = results[["Nazwisko", "Imi�", "Trasa", "Miejsce", "Ur."]]
+        results_better = results[["Nazwisko", "Imi�", "Trasa", "Miejsce", "Ur.", "Płeć"]]
         return results_better
 
     def category_loader(self):
@@ -18,8 +18,33 @@ class DataLoader():
         score = pd.read_csv('score.csv', ';')
         return score
 
+    def general_loader(self):
+        try:
+            general = pd.read_csv('general.csv')
+        except:
+            col = ['Nazwisko', 'Imie', 'Kategoria_wiekowa']
+            general = pd.DataFrame(columns=col)
+        return general
+
+class ScoreCalculator():
+    def method(self, score, category, results, general):
+        for row in results:
+            try:
+                cat = results['Płeć'] + '_' + kategoria 
+                # sprawdzenie kategorii - osobna metoda?
+                # policzenie punktów - osobna metoda?
+                #  a w zasadzie od razu stworzyc dla wszystkich etapów kolumny i wtedy liczenie ogólem jest spoko?
+                pass
+            except:
+                # nowy wiersz jak ktoś jeszcze nie był 
+                pass
+
 if __name__ == "__main__":
     loader = DataLoader()
     results = loader.result_loader('nocny-resuts.csv')
     category = loader.category_loader()
-    print(loader.score_loader())
+    score = loader.score_loader()
+    general = loader.general_loader()
+
+
+    #  dobra musi byc wczytywanie kolejnych resultsów i tworzeni kolejnych kolumn w generalce
